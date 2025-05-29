8e873d1435d4ecfc990afdf136f0b18fc7d433d9
Pedro Souza <pedro.olegario23@gmail.com>
2025-05-28 22:25:03 -0300

    ✨ feat: added restaurant register

--- src/pages/routes.tsx
+++ src/pages/routes.tsx
@@ -5,6 +5,7 @@ import { Dashboard } from "./app/dashboard";
 import { Signin } from "./auth/sign-in";
 import { AppLayout } from "./_layouts/app";
 import { AuthLayout } from "./_layouts/auth";
+import { SignUp } from "./auth/sign-up";
 
 export const router = createBrowserRouter([
   {
@@ -16,6 +17,9 @@ export const router = createBrowserRouter([
   {
     path: '/',
     element: <AuthLayout />,
-    children: [{ path: '/sign-in', element: <Signin /> }],
+    children: [
+      { path: '/sign-in', element: <Signin /> },
+      { path: '/sign-up', element: <SignUp /> }
+    ],
   },
 ])
