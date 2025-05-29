55241cc214272ee0b5d327edf1ef105e61efc8b7
Pedro Souza <pedro.olegario23@gmail.com>
2025-05-29 13:09:10 -0300

    ✨ feat: header completed

--- src/App.tsx
+++ src/App.tsx
@@ -2,15 +2,17 @@ import { RouterProvider } from 'react-router-dom'
 import './global.css'
 import { router } from './pages/routes'
 import { Toaster } from 'sonner'
-
+import { ThemeProvider } from "@/components/theme/theme-provider"
 import { HelmetProvider, Helmet } from 'react-helmet-async'
 
 export function App() {
   return (
     <HelmetProvider>
-      <Helmet titleTemplate='%s | pizza.shop' />
-      <Toaster richColors closeButton  />
-      <RouterProvider router={router} />
+      <ThemeProvider storageKey="pizza-shop-theme" defaultTheme='dark'>
+        <Helmet titleTemplate='%s | pizza.shop' />
+        <Toaster richColors closeButton />
+        <RouterProvider router={router} />
+      </ThemeProvider>
     </HelmetProvider>
   )
 }
