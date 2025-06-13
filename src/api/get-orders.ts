316107eb57c9f049015fecc2b2d80828dac89aa2
Pedro Souza <pedro.olegario23@gmail.com>
2025-06-12 22:39:49 -0300

    ✨ feat: orders pagination

--- src/api/get-orders.ts
+++ src/api/get-orders.ts
@@ -1,5 +1,8 @@
 import { api } from "@/lib/axios"
 
+export interface GetOrdersQuery{
+    pageIndex?: number | null
+}
 
 export interface GetOrdersResponse{
     orders: {
@@ -17,10 +20,10 @@ export interface GetOrdersResponse{
 }
 
 
-export async function getOrders() {
+export async function getOrders({ pageIndex }:GetOrdersQuery) {
     const response = await api.get<GetOrdersResponse>('/orders', {
         params: {
-            pageIndex: 0,
+            pageIndex
         }
     })
 
--- src/pages/app/orders/orders.tsx
+++ src/pages/app/orders/orders.tsx
@@ -5,13 +5,28 @@ import { Pagination } from "@/components/pagination";
 import { Table, TableBody, TableHead, TableHeader, TableRow } from "@/components/ui/table";
 import { useQuery } from "@tanstack/react-query";
 import { Helmet } from "react-helmet-async";
+import { useSearchParams } from "react-router-dom";
+import { z } from "zod";
 
 export function Orders() {
+    const [searchParams, setSearchParams] = useSearchParams()
+
+    const pageIndex = z.coerce.number()
+        .transform(page => page - 1)
+        .parse(searchParams.get('page') ?? '1')
+
     const { data: result } = useQuery({
-        queryKey: ['orders'],
-        queryFn: getOrders
+        queryKey: ['orders', pageIndex],
+        queryFn: () => getOrders({ pageIndex }),
     })
 
+    function handlePaginate(pageIndex: number){
+        setSearchParams(state => {
+            state.set('page', (pageIndex + 1).toString())
+
+            return state
+        })
+    }
     return (
         <>
             <Helmet title='Pedidos' />
@@ -44,7 +59,10 @@ export function Orders() {
                         </Table>
 
                     </div>
-                    <Pagination pageIndex={0} totalCount={105} perPage={10} />
+
+                    {result && (
+                        <Pagination onPageChange={handlePaginate} pageIndex={result.meta.pageIndex} totalCount={result.meta.totalCount} perPage={result.meta.perPage} />
+                    )}
                 </div>
             </div>
         </>
