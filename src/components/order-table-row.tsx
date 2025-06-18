cfbd332c35e88bcd29b9e030d3cc2308d53a1db9
Pedro Souza <pedro.olegario23@gmail.com>
2025-06-17 22:21:45 -0300

    ✨ feat: orders details

--- src/components/order-table-row.tsx
+++ src/components/order-table-row.tsx
@@ -7,6 +7,7 @@ import { OrderStatus } from "./order-status";
 
 import { formatDistanceToNow } from 'date-fns'
 import { ptBR } from 'date-fns/locale'
+import { useState } from "react";
 
 export interface OrderTableRowProps {
     order: {
@@ -19,17 +20,19 @@ export interface OrderTableRowProps {
 }
 
 export function OrderTableRow({ order }: OrderTableRowProps) {
+    const [isDetailsOpen, setIsDetailsOpen] = useState(false)
+
     return (
         <TableRow>
             <TableCell>
-                <Dialog>
+                <Dialog open={isDetailsOpen} onOpenChange={setIsDetailsOpen}>
                     <DialogTrigger asChild>
                         <Button variant='outline' size='xs'>
                             <Search className="h-3 w-3" />
                             <span className="sr-only">Detalhes do pedido</span>
                         </Button>
                     </DialogTrigger>
-                    <OrderDetails />
+                    <OrderDetails open={isDetailsOpen} orderId={order.orderId} />
                 </Dialog>
             </TableCell>
             <TableCell className="font-mono text-xs font-medium">
@@ -48,7 +51,7 @@ export function OrderTableRow({ order }: OrderTableRowProps) {
                 {order.customerName}
             </TableCell>
             <TableCell className="font-medium">
-                {order.total.toLocaleString('pt-BR', {
+                {(order.total / 100).toLocaleString('pt-BR', {
                     style: 'currency',
                     currency: 'BRL'
                 })}
